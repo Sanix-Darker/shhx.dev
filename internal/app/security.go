80db9fce4ba9f1a82adc43ba1516bdeb66e5b2f5
sanix-darker <s4nixd@gmail.com>
2022-03-20 07:20:19 +0000

    Reject cross-origin mutations

--- internal/app/security.go
+++ internal/app/security.go
@@ -51,3 +51,56 @@ type rateLimiter struct {
 	limits  map[string]rateConfig
 	buckets map[string]*rateBucket
 }
+
+func newRateLimiter() *rateLimiter {
+	return &rateLimiter{
+		limits: map[string]rateConfig{
+			"index":   {burst: 60, refillPerSec: 1, cleanupWindow: 15 * time.Minute},
+			"preview": {burst: 30, refillPerSec: 0.5, cleanupWindow: 15 * time.Minute},
+			"create":  {burst: 12, refillPerSec: 0.2, cleanupWindow: 20 * time.Minute},
+			"join":    {burst: 20, refillPerSec: 0.5, cleanupWindow: 20 * time.Minute},
+			"events":  {burst: 20, refillPerSec: 0.5, cleanupWindow: 30 * time.Minute},
+			"signal":  {burst: 180, refillPerSec: 3, cleanupWindow: 20 * time.Minute},
+			"leave":   {burst: 40, refillPerSec: 1, cleanupWindow: 20 * time.Minute},
+			"static":  {burst: 120, refillPerSec: 4, cleanupWindow: 15 * time.Minute},
+			"other":   {burst: 30, refillPerSec: 0.5, cleanupWindow: 15 * time.Minute},
+		},
+		buckets: make(map[string]*rateBucket),
+	}
+}
+
+func (l *rateLimiter) allow(ip, bucketName string, now time.Time) bool {
+	if ip == "" {
+		ip = "unknown"
+	}
+
+	limit, ok := l.limits[bucketName]
+	if !ok {
+		limit = l.limits["other"]
+	}
+
+	key := bucketName + "|" + ip
+
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.pruneLocked(now)
+
+	bucket, ok := l.buckets[key]
+	if !ok {
+		l.buckets[key] = &rateBucket{
+			tokens:   limit.burst - 1,
+			lastSeen: now,
+		}
+		return true
+	}
+
+	elapsed := now.Sub(bucket.lastSeen).Seconds()
+	if elapsed > 0 {
+		bucket.tokens += elapsed * limit.refillPerSec
+		if bucket.tokens > limit.burst {
+			bucket.tokens = limit.burst
+		}
+	}
+	bucket.lastSeen = now
+
