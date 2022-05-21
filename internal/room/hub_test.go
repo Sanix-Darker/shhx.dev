9e33d65b4b94eecf1381c072f5e4ad58c8231c2a
sanix-darker <s4nixd@gmail.com>
2022-05-21 04:41:18 +0000

    Add coverage for room join and leave behavior

--- internal/room/hub_test.go
+++ internal/room/hub_test.go
@@ -51,3 +51,56 @@ func TestHubJoinAndSignalFlow(t *testing.T) {
 		t.Fatalf("expected signal event, got %s", signal.Type)
 	}
 
+	data, ok := signal.Data.(SignalData)
+	if !ok {
+		t.Fatalf("expected SignalData payload, got %T", signal.Data)
+	}
+	if data.From != "owner-1" || data.To != "guest-1" || data.Type != "offer" {
+		t.Fatalf("unexpected signal envelope: %+v", data)
+	}
+	if string(data.Payload) != string(payload) {
+		t.Fatalf("unexpected payload: %s", string(data.Payload))
+	}
+}
+
+func TestHubRoomCapacity(t *testing.T) {
+	hub := NewHub()
+	roomCode := hub.CreateRoom("owner-1", "Owner")
+
+	if err := hub.JoinRoom(roomCode, "guest-1", "Guest"); err != nil {
+		t.Fatalf("first join: %v", err)
+	}
+	if err := hub.JoinRoom(roomCode, "guest-2", "Extra"); err != ErrRoomFull {
+		t.Fatalf("expected ErrRoomFull, got %v", err)
+	}
+}
+
+func TestHubLeaveDeletesEmptyRoom(t *testing.T) {
+	hub := NewHub()
+	roomCode := hub.CreateRoom("owner-1", "Owner")
+
+	hub.Leave(roomCode, "owner-1")
+
+	if _, _, err := hub.Subscribe(roomCode, "owner-1"); err != ErrRoomNotFound {
+		t.Fatalf("expected room deletion, got %v", err)
+	}
+}
+
+func TestHubPruneExpiredRooms(t *testing.T) {
+	hub := NewHub()
+	hub.roomTTL = time.Minute
+
+	roomCode := hub.CreateRoom("owner-1", "Owner")
+
+	hub.mu.Lock()
+	hub.rooms[roomCode].lastActive = time.Now().Add(-2 * time.Minute)
+	hub.mu.Unlock()
+
+	hub.pruneExpired(time.Now())
+
+	if _, _, err := hub.Subscribe(roomCode, "owner-1"); err != ErrRoomNotFound {
+		t.Fatalf("expected room pruning, got %v", err)
+	}
+}
+
+func expectEvent(t *testing.T, ch <-chan Event) Event {
